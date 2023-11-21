cbee4c878d772581ee946c301f74e7d475a79d40
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-11-21 11:23:11 +0900

    feat: image blur처리 및 default image 수정

--- components/image-card.tsx
+++ components/image-card.tsx
@@ -8,13 +8,16 @@ const ImageCard = ({ href, id, selectId, setSelectId }: ImageCardProps) => {
     <Image
       className={cn(
         "rounded-3xl",
-        id === selectId ? "border-2 border-[#F9E06C]" : "opacity-50"
+        id === selectId ? "border-2 border-[#F9E06C]" : "opacity-50",
+        selectId === 0 ? "opacity-100" : ""
       )}
       src={href}
       width={200}
       height={200}
       alt="image"
       onClick={() => setSelectId(id)}
+      placeholder="blur"
+      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
     />
   );
 };
