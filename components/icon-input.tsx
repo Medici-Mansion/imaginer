42002e03989746ef8090f253ab97df1c6d63b4da
LeeBonHoon <80507816+LeeBonHoon1@users.noreply.github.com>
2023-11-23 00:22:51 +0900

    [CLIENT] API요청 시 로딩상태 추가 (#13)
    * [CLIENT] input loading 상태 추가
    
    * feat: loading 상태 시 disabled 조건 추가
    
    * fix: 주석 제거
    
    ---------
    
    Co-authored-by: Raymond <akdfhr2@gmail.com>

--- components/icon-input.tsx
+++ components/icon-input.tsx
@@ -10,13 +10,15 @@ import {
   FormItem,
   FormLabel,
 } from "@/components/ui/form";
+import { RotateCw } from "lucide-react";
 
 interface IconInputProps {
   form: UseFormReturn<FormData>;
   label?: string;
+  loading?: boolean;
 }
 
-const IconInput = ({ form, label }: IconInputProps) => {
+const IconInput = ({ form, label, loading }: IconInputProps) => {
   return (
     <FormField
       control={form.control}
@@ -32,24 +34,29 @@ const IconInput = ({ form, label }: IconInputProps) => {
                 placeholder="Type your subject"
                 {...field}
                 value={field.value || ""}
+                disabled={loading}
                 className="m-auto w-[40%] bg-[#110F19] py-7 rounded-full border-none"
               />
             </FormControl>
             <button
               className="absolute right-[31%] top-1/2 pr-2 hover:cursor-pointer -translate-y-1/2"
               type={"submit"}
-              disabled={!form.getValues().sentence}
+              disabled={!form.getValues().sentence || loading}
             >
-              <Image
-                src={
-                  !form.getValues().sentence
-                    ? "/images/send.png"
-                    : "/images/send_fill.png"
-                }
-                width={20}
-                height={20}
-                alt="send"
-              />
+              {loading ? (
+                <RotateCw className="animate-spin w-5 h-5" />
+              ) : (
+                <Image
+                  src={
+                    !form.getValues().sentence
+                      ? "/images/send.png"
+                      : "/images/send_fill.png"
+                  }
+                  width={20}
+                  height={20}
+                  alt="send"
+                />
+              )}
             </button>
           </div>
         </FormItem>
