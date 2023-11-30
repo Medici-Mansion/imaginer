8a76524ff9f88dded0f68448a29d76af77915e51
raymondanything <akdfhr2@gmail.com>
2023-11-30 17:22:24 +0900

    fix: generate 프롬프트 블록 사이즈 변경

--- app/(routes)/(complete)/layout.tsx
+++ app/(routes)/(complete)/layout.tsx
@@ -5,10 +5,7 @@ import Prompt from "@/components/prompt";
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="grow flex flex-col">
-      <Prompt
-        cache
-        className="py-5 mx-auto flex-[0.5] flex items-center w-3/4"
-      />
+      <Prompt cache className="py-5 mx-auto flex-[0.5] flex items-center" />
       <main className="flex-[3] flex flex-col justify-center">{children}</main>
       <AuthProvider />
     </div>
