2b2fd1cbaab2946408615d454c742a0ff874866b
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2024-01-07 22:47:22 +0900

    feat: 캐러셀 화살표 스타일 변경

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -201,7 +201,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-24 w-12 rounded-md bg-[#FFFFFF26]",
+        "absolute rounded-full border-white hover:bg-transparent",
         orientation === "horizontal"
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -230,7 +230,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-24 w-12 rounded-md bg-[#FFFFFF26] hover:bg-[#FFFFFF26]",
+        "absolute rounded-full border-white hover:bg-transparent",
         orientation === "horizontal"
           ? "-right-12 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
